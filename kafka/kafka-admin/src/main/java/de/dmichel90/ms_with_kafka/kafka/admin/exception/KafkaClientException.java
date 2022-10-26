3bc8c6607b542112ef950c10eed4cd19f10953ef
Daniel Michel <danielmichel1190@gmail.com>
2022-10-26 23:44:19 +0200

    added kafka module with sub-modules kafka-admin, kafka-model, kafka-producer (WIP)

---
+++ kafka/kafka-admin/src/main/java/de/dmichel90/ms_with_kafka/kafka/admin/exception/KafkaClientException.java
@@ -0,0 +1,18 @@
+package de.dmichel90.ms_with_kafka.kafka.admin.exception;
+
+/**
+ * Exception class for Kafka client error situations.
+ */
+public class KafkaClientException extends RuntimeException {
+
+    public KafkaClientException() {
+    }
+
+    public KafkaClientException(String message) {
+        super(message);
+    }
+
+    public KafkaClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
